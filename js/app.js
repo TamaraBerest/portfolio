bda11f1a5a94a7774f3c35830b031468c834e261
Tamara <toma4beerest@gmail.com>
2023-02-26 17:26:08 +0100

    try iphone 3

--- js/app.js
+++ js/app.js
@@ -1,53 +1,53 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+// gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
 
-if (ScrollTrigger.isTouch !== 1) {
-    ScrollSmoother.create({
-        wrapper: '.wrapper',
-        content: '.content',
-        smooth: 1.5,
-        effects: true
-    })
+// if (ScrollTrigger.isTouch !== 1) {
+//     ScrollSmoother.create({
+//         wrapper: '.wrapper',
+//         content: '.content',
+//         smooth: 1.5,
+//         effects: true
+//     })
 
-    gsap.fromTo('.hero-section', { opacity: 1 }, {
-        opacity: 0,
-        scrollTrigger: {
-            trigger: '.hero-section',
-            start: 'center',
-            end: '820',
-            scrub: true
-        }
-    })
+//     gsap.fromTo('.hero-section', { opacity: 1 }, {
+//         opacity: 0,
+//         scrollTrigger: {
+//             trigger: '.hero-section',
+//             start: 'center',
+//             end: '820',
+//             scrub: true
+//         }
+//     })
 
-    let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+//     let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
 
-    itemsL.forEach(item => {
-        gsap.fromTo(item, { x: -50, opacity: 0 }, {
-            opacity: 1, x: 0,
-            scrollTrigger: {
-                trigger: item,
-                start: '-850',
-                end: '-100',
-                scrub: true
-            }
-        })
-    })
+//     itemsL.forEach(item => {
+//         gsap.fromTo(item, { x: -50, opacity: 0 }, {
+//             opacity: 1, x: 0,
+//             scrollTrigger: {
+//                 trigger: item,
+//                 start: '-850',
+//                 end: '-100',
+//                 scrub: true
+//             }
+//         })
+//     })
 
-    let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+//     let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
 
-    itemsR.forEach(item => {
-        gsap.fromTo(item, { x: 50, opacity: 0 }, {
-            opacity: 1, x: 0,
-            scrollTrigger: {
-                trigger: item,
-                start: '-850',
-                end: '-100',
-                scrub: true
-            }
-        })
-    })
+//     itemsR.forEach(item => {
+//         gsap.fromTo(item, { x: 50, opacity: 0 }, {
+//             opacity: 1, x: 0,
+//             scrollTrigger: {
+//                 trigger: item,
+//                 start: '-850',
+//                 end: '-100',
+//                 scrub: true
+//             }
+//         })
+//     })
 
-}
+// }
 
 const menuBtn = document.querySelector(".page__menu");
 const menu = document.querySelector(".page__menu_bg");
@@ -68,9 +68,9 @@ menuBtn.addEventListener("click", () => {
 });
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function () {
-    console.log('callback - particles.js config loaded');
-});
+// particlesJS.load('particles-js', 'assets/particles.json', function () {
+//     console.log('callback - particles.js config loaded');
+// });
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
