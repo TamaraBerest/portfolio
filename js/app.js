4de376bfd5e34202405b59c93a493aff083c5ea2
Tamara <toma4beerest@gmail.com>
2023-05-02 16:08:01 +0200

    about1

--- js/app.js
+++ js/app.js
@@ -8,44 +8,46 @@ if (ScrollTrigger.isTouch !== 1) {
         smooth: 1.5,
         effects: true
     })
+    if (document.querySelector('.hero-section')) {
 
-    gsap.fromTo('.hero-section', { opacity: 1 }, {
-        opacity: 0,
-        scrollTrigger: {
-            trigger: '.hero-section',
-            start: 'center',
-            end: '820',
-            scrub: true
-        }
-    })
-
-    let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
-
-    itemsL.forEach(item => {
-        gsap.fromTo(item, { x: -50, opacity: 0 }, {
-            opacity: 1, x: 0,
+        gsap.fromTo('.hero-section', { opacity: 1 }, {
+            opacity: 0,
             scrollTrigger: {
-                trigger: item,
-                start: '-850',
-                end: '-100',
+                trigger: '.hero-section',
+                start: 'center',
+                end: '820',
                 scrub: true
             }
         })
-    })
 
-    let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+        let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
 
-    itemsR.forEach(item => {
-        gsap.fromTo(item, { x: 50, opacity: 0 }, {
-            opacity: 1, x: 0,
-            scrollTrigger: {
-                trigger: item,
-                start: '-850',
-                end: '-100',
-                scrub: true
-            }
+        itemsL.forEach(item => {
+            gsap.fromTo(item, { x: -50, opacity: 0 }, {
+                opacity: 1, x: 0,
+                scrollTrigger: {
+                    trigger: item,
+                    start: '-850',
+                    end: '-100',
+                    scrub: true
+                }
+            })
         })
-    })
+
+        let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+
+        itemsR.forEach(item => {
+            gsap.fromTo(item, { x: 50, opacity: 0 }, {
+                opacity: 1, x: 0,
+                scrollTrigger: {
+                    trigger: item,
+                    start: '-850',
+                    end: '-100',
+                    scrub: true
+                }
+            })
+        })
+    }
 
 }
 
@@ -71,7 +73,7 @@ menuBtn.addEventListener("click", () => {
 });
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function () {
+particlesJS.load('particles-js', '../assets/particles.json', function () {
     console.log('callback - particles.js config loaded');
 });
 
@@ -100,15 +102,17 @@ particlesJS.load('particles-js', 'assets/particles.json', function () {
 //     // },
 // });
 // const portfolio = document.querySelector(".gallery");
-
 const buttonScroll = document.querySelector(".main-header__scroll_button");
-buttonScroll.addEventListener("click", () => {
-    // portfolio.scrollIntoView({
-    //     block: "center",
-    //     behavior: "smooth",
-    // });
-    window.scrollTo(0, document.documentElement.clientHeight * 1);
-})
+
+if (buttonScroll) {
+    buttonScroll.addEventListener("click", () => {
+        // portfolio.scrollIntoView({
+        //     block: "center",
+        //     behavior: "smooth",
+        // });
+        window.scrollTo(0, document.documentElement.clientHeight * 1);
+    })
+}
 
 document.addEventListener('touchmove', function (event) {
     if (event.scale !== 1) { event.preventDefault(); }
@@ -117,36 +121,39 @@ document.addEventListener('touchmove', function (event) {
 const sliderItems = document.querySelectorAll(".slider__slide");
 const sliderNext = document.querySelector(".slider__arrows_next");
 const sliderBack = document.querySelector(".slider__arrows_back");
-sliderNext.addEventListener("click", () => {
-    for (let i = 0; i < sliderItems.length; i++) {
-        if (sliderItems[i].classList.contains("active")) {
-            if (i === sliderItems.length - 1) {
-                sliderItems[4].classList.remove("active");
-                sliderItems[0].classList.add("active");
-                break
-            }
-            else {
-                sliderItems[i].classList.remove("active");
-                sliderItems[i + 1].classList.add("active");
-                break
+if (sliderItems.length > 0) {
+    sliderNext.addEventListener("click", () => {
+        for (let i = 0; i < sliderItems.length; i++) {
+            if (sliderItems[i].classList.contains("active")) {
+                if (i === sliderItems.length - 1) {
+                    sliderItems[4].classList.remove("active");
+                    sliderItems[0].classList.add("active");
+                    break
+                }
+                else {
+                    sliderItems[i].classList.remove("active");
+                    sliderItems[i + 1].classList.add("active");
+                    break
+                }
             }
         }
-    }
-})
+    })
 
-sliderBack.addEventListener("click", () => {
-    for (let i = 0; i < sliderItems.length; i++) {
-        if (sliderItems[i].classList.contains("active")) {
-            if (i === 0) {
-                sliderItems[0].classList.remove("active");
-                sliderItems[4].classList.add("active");
-                break
-            }
-            else {
-                sliderItems[i].classList.remove("active");
-                sliderItems[i - 1].classList.add("active");
-                break
+    sliderBack.addEventListener("click", () => {
+        for (let i = 0; i < sliderItems.length; i++) {
+            if (sliderItems[i].classList.contains("active")) {
+                if (i === 0) {
+                    sliderItems[0].classList.remove("active");
+                    sliderItems[4].classList.add("active");
+                    break
+                }
+                else {
+                    sliderItems[i].classList.remove("active");
+                    sliderItems[i - 1].classList.add("active");
+                    break
+                }
             }
         }
-    }
-})
+    })
+
+}
